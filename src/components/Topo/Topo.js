45abb092ed5b147ec4d8d4d672a9b1b10dbcd12e
Marcus Pontes <marcusporama@gmail.com>
2021-08-25 10:20:47 -0300

    inclusão de medalhas no topo

--- src/components/Topo/Topo.js
+++ src/components/Topo/Topo.js
@@ -1,7 +1,14 @@
 import { TopoBar } from "./styles"
+import { FaMedal } from "react-icons/fa"
 
 function Topo() {
-  return <TopoBar>Quadro de medalhas</TopoBar>
+  return (
+    <TopoBar>
+      Quadro de medalhas <FaMedal color="gold" />
+      <FaMedal color="silver" />
+      <FaMedal color="#e67300" />
+    </TopoBar>
+  )
 }
 
 export default Topo
