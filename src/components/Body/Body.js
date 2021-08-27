b884e0ab593d62382545e74b37c0ae8e53f5e577
Marcus Pontes <marcusporama@gmail.com>
2021-08-27 07:36:45 -0300

    update project

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,5 +1,122 @@
+import { Table, Th, Tr, Td, Busca } from "./styles"
+
+import { medalists } from "../api/medalists"
+import { useEffect, useState } from "react"
+
+import { Button, Modal } from "react-bootstrap"
+
 function Body() {
-  return <h1>Body</h1>
+  const [results, setResults] = useState({})
+  const [search, setSearch] = useState("")
+  const [show, setShow] = useState(false)
+  const [details, setDetails] = useState(null)
+
+  const handleClose = () => setShow(false)
+
+  useEffect(() => {
+    getDadosOrdered()
+  }, [search])
+
+  function handleShow(details) {
+    setShow(true)
+    setDetails(details)
+  }
+  function getDadosOrdered() {
+    const standings = {}
+
+    medalists.forEach((item) => {
+      const { country, medal, athlete: name, event, sex } = item
+      if (!standings[country]) {
+        standings[country] = {
+          medals: {
+            gold: 0,
+            silver: 0,
+            bronze: 0,
+            total: 0,
+          },
+          atletes: [],
+        }
+      }
+      standings[country].medals[medal.toLowerCase()]++
+      standings[country].medals.total++
+      const athlete = { name, event, sex }
+      standings[country].atletes.push(athlete)
+    })
+
+    const sortedStandings = Object.entries(standings).sort(
+      ([_, a], [__, b]) => b.medals.total - a.medals.total
+    )
+
+    const filterednames = sortedStandings.filter((names) => {
+      return names[0].toLowerCase().includes(search.toLowerCase())
+    })
+    setResults(filterednames)
+  }
+
+  return (
+    <div>
+      <Busca>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => {
+            setSearch(e.target.value)
+          }}
+        />
+      </Busca>
+      <Table>
+        <Tr>
+          <Th>País</Th>
+          <Th>Ouro</Th>
+          <Th>Prata</Th>
+          <Th>Bronze</Th>
+          <Th>Total</Th>
+        </Tr>
+        {results.length > 0 &&
+          results.map(([name, country]) => (
+            <Tr key={name}>
+              <Td>
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    handleShow(country)
+                  }}
+                >
+                  {name}
+                </Button>
+              </Td>
+              <Td>{country.medals.gold}</Td>
+              <Td>{country.medals.silver}</Td>
+              <Td>{country.medals.bronze}</Td>
+              <Td>{country.medals.total}</Td>
+            </Tr>
+          ))}
+        {details && (
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Atletas:</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>
+                {details.atletes.map((name) => {
+                  return (
+                    <p>
+                      <strong>Name: {name.name} </strong>
+                      <p>
+                        Event: {name.event}
+                        <p>Sex: {name.sex}</p>
+                      </p>
+                    </p>
+                  )
+                })}
+                <p>Total medals: {details.medals.total}</p>
+              </p>
+            </Modal.Body>
+          </Modal>
+        )}
+      </Table>
+    </div>
+  )
 }
 
 export default Body
---
+++ src/components/Body/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+export const Table = styled.table`
+  font-family: "Roboto";
+  font-weight: 600;
+  border-collapse: collapse;
+  width: 70%;
+  margin-top: 15px;
+  color: #333;
+`
+export const Td = styled.td`
+  padding: 15px;
+  text-align: center;
+`
+
+export const Th = styled.th`
+  padding: 15px;
+`
+
+export const Tr = styled.tr`
+  font-size: 18px;
+  text-transform: uppercase;
+  border-bottom: 1px solid #ddd;
+`
+
+export const Medals = styled.div``
+
+export const TBody = styled.tbody``
+
+export const Busca = styled.div`
+  input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 9px 0px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  padding: 15px;
+`
