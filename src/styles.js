17882bbe64e494be265f5d0cd84081904d2b6834
Marcus Pontes <marcusporama@gmail.com>
2021-08-28 02:12:12 -0300

    update styles.js

--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,6 @@ export const Container = styled.div`
 `
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,600');  
 
   html, body {
     font-family: Roboto, sans-serif;
